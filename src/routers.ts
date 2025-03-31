733ad21062cc1568bacf985fa453ba9a92030064
yusley <yusley.santos23@gmail.com>
2025-03-31 13:34:50 -0500

    configuracoes iniciais

---
+++ src/routers.ts
@@ -0,0 +1,7 @@
+import { Router, Request,Response,NextFunction } from "express";
+
+const router = Router()
+
+router.get('/', (req:Request ,res:Response, next: NextFunction) => {
+    res.status(200)
+})
