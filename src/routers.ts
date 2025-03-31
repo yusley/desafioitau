39a2b0071a08342920a82c61cc8af6d6c527d752
yusley <yusley.santos23@gmail.com>
2025-03-31 14:39:19 -0500

    chore: add typescript config

--- src/routers.ts
+++ src/routers.ts
@@ -2,6 +2,9 @@ import { Router, Request,Response,NextFunction } from "express";
 
 const router = Router()
 
+
 router.get('/', (req:Request ,res:Response, next: NextFunction) => {
     res.status(200)
 })
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express'
+import router from './routers';
+import { json } from 'express';
 
 const app = express();
 
 const port = '3000'
 
+app.use(json());
+
+app.use(router);
 
 app.listen(port,() => {
     console.log('Server runing in port ', port)
