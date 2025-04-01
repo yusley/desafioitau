f9e71f13bf8f87914a61a165c87454ff1ecd6ad0
yusley <yusley.santos23@gmail.com>
2025-04-01 10:07:09 -0500

    test jest

--- src/services/index.spec.ts
+++ src/services/index.spec.ts
@@ -2,5 +2,5 @@ import ListTransactionService from "./ListTransactionsService"
 
 it("should sum", () => {
   const listTransactionService = new ListTransactionService()
-  expect(listTransactionService.execute()).toBe("transacao")
+  expect(listTransactionService.execute()).toBe("transacao!")
 })
