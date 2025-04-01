29ed98f71deb5891384a0e3f370b5657b1902f5e
yusley <yusley.santos23@gmail.com>
2025-04-01 07:24:25 -0500

    chore: adicao do eslint e prettier and nodemon

--- src/routers.ts
+++ src/routers.ts
@@ -1,10 +1,13 @@
-import { Router, Request,Response,NextFunction } from "express";
+import { Router, Request, Response } from "express"
 
 const router = Router()
 
-
-router.get('/', (req:Request ,res:Response, next: NextFunction) => {
-    res.status(200)
+router.get("/", (req: Request, res: Response) => {
+  res.status(200)
 })
 
-export default router;
+router.get("/trasaction", (req: Request, res: Response) => {
+  res.status(200)
+})
+
+export default router
